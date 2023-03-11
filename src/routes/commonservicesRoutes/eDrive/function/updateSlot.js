8405041b9b8669c73aaa9e9755cc4390af8751b5
Pinidu Maneesha <pmjuniorcomtec@gmail.com>
2023-03-11 21:51:50 +0530

    updator updated

--- src/routes/commonservicesRoutes/eDrive/function/updateSlot.js
+++ src/routes/commonservicesRoutes/eDrive/function/updateSlot.js
@@ -1,12 +1,23 @@
 import user_dataDB from "../../../../contollers/user_dataDBconnection.js";
 
-const updateSlot = (userId, slotId, newValue) => {
+const updateSlot = async (userId, slotId, newValue) => {
+  const slotIds = ["slot1", "slot2", "slot3", "slot4", "slot5"];
+  if (!slotIds.includes(slotId)) {
+    return;
+  }
 
-  const sql = "UPDATE saved_writings SET " + slotId +" = ? WHERE user_id = ? LIMIT 1 ;"
+  try {
+    const sql = `UPDATE saved_writings SET ${slotId} = ? WHERE user_id = ? LIMIT 1 ;`;
+    const values = [newValue, userId];
 
-  const values = [newValue, userId];
-
-  user_dataDB.query(sql, values).catch((err) => console.log(err)); //not necessary
+    const result = await user_dataDB.query(sql, values);
+    console.log(result[0].affectedRows);
+    if (result[0].affectedRows === 0) {
+      const sql = `INSERT INTO saved_writings(user_id, ${slotId}) VALUES(?, ?);`;
+      const values = [userId, newValue];
+      user_dataDB.query(sql, values).then((r) => console.log(r));
+    }
+  } catch (err){console.log(err)};
 };
 
 export default updateSlot;
