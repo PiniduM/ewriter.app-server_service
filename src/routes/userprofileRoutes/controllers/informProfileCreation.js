07912c59119b64370fc0af127d47d72a590f0e18
Pinidu Maneesha <pmjuniorcomtec@gmail.com>
2023-03-15 16:08:05 +0530

    updated

--- src/routes/userprofileRoutes/controllers/informProfileCreation.js
+++ src/routes/userprofileRoutes/controllers/informProfileCreation.js
@@ -3,8 +3,11 @@ import axios from "axios";
 const informProfileCreation = async (loginToken) => {
   const ulr = process.env.URL_SERVER_SERVICE_ROOT + "/s5002/profilecreated";
 
+  const config = {
+    headers:{"Content-Legth": "67"}
+  }
   try {
-    const result = await axios.post(ulr, { loginToken });
+    const result = await axios.post(ulr, { loginToken },config);
     return result.data;
   } catch (err) {
     return err;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express,{ json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import axios from "axios";
 
 import userProfileRouter from "./routes/userProfileRoutes/userProfileRouter.js";
 import eDriveRouter from "./routes/commonservicesRoutes/eDrive/eDriveRouter.js";
@@ -14,6 +15,19 @@ const app = express();
 app.use(cors());
 app.use(json());
 
+app.get("/test",(req,res) => {
+
+    const config = {headers :{
+        a: "hello",
+        origin: "http://localhost:3000",
+       // host: "http://localhost:3000"
+    }}
+    const url = `http://localhost:5001/auth/login`;
+    axios
+      .post(url, {identifier: "abcd2334",password: "1234gsdg"},config)
+      .then(result => res.send(result))
+      .catch(err => res.send(err.response.data));
+})
 
 app.use("/profile",userProfileRouter);
 
